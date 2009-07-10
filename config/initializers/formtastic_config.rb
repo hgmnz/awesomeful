f970f0ebefb88132eb853e481161943b07bb54da
hgimenez <harold.gimenez@gmail.com>
2009-07-10 16:52:35 -0400

    Some formtastic defaults

---
+++ config/initializers/formtastic_config.rb
@@ -0,0 +1,6 @@
+# Set the default text field size when input is a string. Default is 50
+Formtastic::SemanticFormBuilder.default_text_field_size = 200
+
+# Should all fields be considered "required" by default
+# Defaults to true, see ValidationReflection notes below
+Formtastic::SemanticFormBuilder.all_fields_required_by_default = false
