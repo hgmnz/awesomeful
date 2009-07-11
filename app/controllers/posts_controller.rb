5199c40145a7e2f4b5206aca40f735d5acae81a3
hgimenez <harold.gimenez@gmail.com>
2009-07-11 19:50:57 -0400

    Added comments to posts controller, style stolen from fat free CRM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,33 @@
 class PostsController < ApplicationController
 
+  # GET /posts/new                        HTML
+  # ------------------------------------------
   def new
     @post = Post.new
   end
 
+  # GET /posts                            HTML
+  # ------------------------------------------
   def index
     @posts       = Post.published
     @latest_post = @posts.slice!(0)
     @post_drafts = Post.drafts
   end
 
+  # GET /posts/:post_id                   HTML
+  # ------------------------------------------
   def show
     @post = Post.find(params[:id])
   end
 
+  # GET /posts/:post_id/edit              HTML
+  # ------------------------------------------
   def edit
     @post = Post.find(params[:id])
   end 
 
+  # POST /posts/:post_id                  HTML
+  # ------------------------------------------
   def update
     @post = Post.find(params[:id])
     if @post.update_attributes(params[:post])
@@ -28,6 +38,8 @@ class PostsController < ApplicationController
     end
   end
 
+  # POST  /posts                         HTML
+  # ------------------------------------------
   def create
     if Post.create!(params[:post])
       flash[:notice] = 'Post created successfully'
@@ -36,9 +48,6 @@ class PostsController < ApplicationController
       flash[:error] = 'Error creating post'
     end
 
-  end
-
-  def destroy
   end
 
 end
