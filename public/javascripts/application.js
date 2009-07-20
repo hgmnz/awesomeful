3bd82a9d86063ae81a7bc1dc8737d4dfebf77e48
hgimenez <harold.gimenez@gmail.com>
2009-07-20 18:23:58 -0400

    Added jquery tweet plugin. Not sure this will fly though

---
+++ public/javascripts/application.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".tweets").tweet({
+        username: "hgimenez",
+        join_text: "auto",
+        avatar_size: 32,
+        count: 4,
+        //auto_join_text_default: "",
+        //auto_join_text_ed: "",
+        //auto_join_text_ing: "we were",
+        //auto_join_text_reply: "I replied to",
+        //auto_join_text_url: "we were checking out",
+        join_text: false,
+        loading_text: "loading tweets..."
+    });
+});
+
