fe499c4b2e04ef30b734e57a4adf7ecd80e52bf4
hgimenez <harold.gimenez@gmail.com>
2009-07-26 21:01:03 -0400

    truncate_html does the right thing with ellipsis at end

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -1,22 +1,32 @@
-# By Henrik Nyh <http://henrik.nyh.se> 2008-01-30.
+# By Henrik Nyh  2008-01-30.
 # Free to modify and redistribute with credit.
-# Source: http://pastie.textmate.org/145402
 
-require "rubygems"
-require "hpricot"
+# modified by Dave Nolan http://textgoeshere.org.uk 
+# Ellipsis appended to text of last HTML node
+# Ellipsis inserted after final word break
+# source http://pastie.textmate.org/148304
+
+require 'hpricot'
 
 module TextHelper
 
   # Like the Rails _truncate_ helper but doesn't break HTML tags or entities.
-  def truncate_html(text, max_length = 30, ellipsis = "")
+  def truncate_html(text, max_length = 30, ellipsis = "...")
     return if text.nil?
 
     doc = Hpricot(text.to_s)
-    ellipsis_length = Hpricot(ellipsis).inner_text.mb_chars.length
-    content_length = doc.inner_text.mb_chars.length
+    ellipsis_length = Hpricot(ellipsis).inner_text.chars.length
+    content_length = doc.inner_text.chars.length
     actual_length = max_length - ellipsis_length
 
-    content_length > max_length ? doc.truncate(actual_length).inner_html + ellipsis : text.to_s
+    if content_length > max_length
+      truncated_doc = doc.truncate(actual_length)
+      last_child = truncated_doc.children.last
+      last_child.inner_html = last_child.inner_html.gsub(/\W.[^\s]+$/, "") + ellipsis
+      truncated_doc
+    else
+      text.to_s
+    end
   end
 
 end
@@ -24,11 +34,11 @@ end
 module HpricotTruncator
   module NodeWithChildren
     def truncate(max_length)
-      return self if inner_text.mb_chars.length <= max_length
+      return self if inner_text.chars.length <= max_length
       truncated_node = self.dup
       truncated_node.children = []
       each_child do |node|
-        remaining_length = max_length - truncated_node.inner_text.mb_chars.length
+        remaining_length = max_length - truncated_node.inner_text.chars.length
         break if remaining_length == 0
         truncated_node.children << node.truncate(remaining_length)
       end
